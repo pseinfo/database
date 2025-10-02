9b7439ed06c3808e1e8b46da2de2c7816ed32036
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-10-02 11:56:08 +0200

    adding element type (prototypal)

---
+++ types/element.ts
@@ -0,0 +1,12 @@
+
+export const Elements = [
+    "ac", "ag", "al", "am", "ar", "as", "at", "au", "b", "ba", "be", "bh", "bi", "bk", "br",
+    "c", "ca", "cd", "ce", "cf", "cl", "cm", "cn", "co", "cr", "cs", "cu", "db", "ds", "dy",
+    "er", "es", "eu", "f", "fe", "fl", "fm", "fr", "ga", "gd", "ge", "h", "he", "hf", "hg",
+    "ho", "hs", "i", "in", "ir", "k", "kr", "la", "li", "lr", "lu", "lv", "mc", "md", "mg", 
+    "mn", "mo", "mt", "n", "na", "nb", "nd", "ne", "nh", "ni", "no", "np", "o", "og", "os",
+    "p", "pa", "pb", "pd", "pm", "po", "pr", "pt", "pu", "ra", "rb", "re", "rf", "rg", "rh",
+    "rn", "ru", "s", "sb", "sc", "se", "sg", "si", "sm", "sn", "sr", "ta", "tb", "tc", "te",
+    "th", "ti", "tl", "tm", "ts", "u", "v", "w", "xe", "y", "yb", "zn", "zr"
+] as const
+export type Elements = (typeof Elements)[number]
