e9a26b0af86a8278eed40f3ce67ce833a925c535
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-10-02 16:03:50 +0200

    sucessfully failed the validation, that means that the schema system checking works

--- automation-src/verify_data.ts
+++ automation-src/verify_data.ts
@@ -1,23 +1,36 @@
 
 import {join} from "path"
 import { ElementSymbols } from "types/elements_symbols"
+import { Ajv} from "ajv"
 
 const DATA_PATH = join(__dirname, "../data/")
+const SCHEMA_AUTOGEN_PATH = join(__dirname, "../schema/autogen/")
 
 async function verify_elements_index_json(){
     const index_json_path = join(DATA_PATH, "elements/_index.json")
-    try {
-        const index_json: string[] = await Bun.file(index_json_path).json()
-        if (JSON.stringify(index_json) != JSON.stringify(ElementSymbols))
-            throw `the list of symbols elements in ${index_json_path} doesn't correlate with the type`
-        
-    } catch(err){
-        throw err
-    }
-    
+    const index_json: string[] = await Bun.file(index_json_path).json()
+    if (JSON.stringify(index_json) != JSON.stringify(ElementSymbols))
+        throw `The list of symbols elements in ${index_json_path} doesn't correlate with the type`
 }
 
+async function schema_validate_elements_atomic() {
+    const schema = await Bun.file(join(SCHEMA_AUTOGEN_PATH, 'atomic.schema.json')).json()
+    const ajv = new Ajv() // reminder that some options can be passed here
+    const validate = ajv.compile(schema)
+    for (const symbols_id of ElementSymbols){
+        const atomic_path = join(DATA_PATH, `elements/${symbols_id}/atomic.json`)
+        const atomic_json = Bun.file(atomic_path).json()
+        const is_valid = validate(atomic_json)
+        if (!is_valid){
+            console.error(`Element ${symbols_id} at ${atomic_path} data doesn't have valid format`)
+            throw validate.errors
+        }
+            
+    }
+}
+
+// if this script is being directly executed and not imported
 if (import.meta.path === Bun.main) {
-    // this script is being directly executed and not imported
     await verify_elements_index_json()
+    await schema_validate_elements_atomic()
 }
