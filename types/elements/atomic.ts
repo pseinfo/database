363398a78f982b9ca879c62514f711c939176781
ForwardFeed <76596109+ForwardFeed@users.noreply.github.com>
2025-10-02 12:06:06 +0200

    small fixes on that configuration and added a prototypical atomic type

---
+++ types/elements/atomic.ts
@@ -0,0 +1,43 @@
+
+
+export type Atomic = {
+  "@metadata": {
+    lastModified: string // ISO date 8610
+    schemaVersion: string
+  },
+  electron_config: string,
+  ionization: {
+    unit: "eV",
+    values: number[]
+  },
+  mass: {
+    atomic_mass: {
+      unit: "u",
+      value: number
+    },
+    standard_atomic_weight: {
+      deviation: number,
+      value: number
+    }
+  },
+  radius: {
+    calculated: {
+      unit: "pm",
+      value: number
+    },
+    covalent: {
+      deviation: number,
+      unit: "pm",
+      value: number
+    },
+    empirical: {
+      unit: "pm",
+      value: number
+    },
+    vdw: {
+      unit: "pm",
+      value: number
+    }
+  },
+  shell: number[]
+}
